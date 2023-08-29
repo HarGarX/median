c3da9c4f3c262c3669366cf6a62ce047dd40a900
HargarX <hargarx@gmail.com>
2023-08-29 16:13:04 +0300

    adding int parse pipe

--- src/articles/dto/create-article.dto.ts
+++ src/articles/dto/create-article.dto.ts
@@ -1,12 +1,33 @@
 import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsString,
+  IsNotEmpty,
+  IsOptional,
+  IsBoolean,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class CreateArticleDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(5)
   @ApiProperty()
   title: string;
+
+  @IsString()
+  @IsOptional()
+  @MaxLength(500)
   @ApiProperty({ required: false })
   description?: string;
+
+  @IsString()
+  @IsNotEmpty()
   @ApiProperty()
   body: string;
+
+  @IsBoolean()
+  @IsOptional()
   @ApiProperty({ required: false })
   published?: boolean = false;
 }
