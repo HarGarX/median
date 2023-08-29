fa372298482d482082cc4b22aada16769c408eab
HargarX <hargarx@gmail.com>
2023-08-29 16:54:35 +0300

    adding logging middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ArticlesModule } from './articles/articles.module';
+import { PrismaModule as Ps, loggingMiddleware } from 'nestjs-prisma';
 
 @Module({
-  imports: [PrismaModule, ArticlesModule],
+  imports: [
+    PrismaModule,
+    ArticlesModule,
+    Ps.forRoot({
+      prismaServiceOptions: {
+        middlewares: [loggingMiddleware()],
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
